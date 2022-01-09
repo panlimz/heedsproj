6b07a0adb57978e49b68e61bba2cc47a9c7cad7d
panlimz <palomalimeira@gmail.com>
2022-01-09 00:46:16 -0300

    Updating settings.py

--- heedsapiproj/settings.py
+++ heedsapiproj/settings.py
@@ -15,12 +15,10 @@ import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
